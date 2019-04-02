2505a4a26e00cae19c678767769b9448def197f5
Pietro Flemming <48135556+Glov3r@users.noreply.github.com>
2019-04-02 09:05:09 -0300

    Fim

---
+++ Exercício15.php
@@ -0,0 +1,33 @@
+<?php
+print "Digite o 1º número: ";
+$numero1= (float) fgets(STDIN);
+
+print "Digite o 2º número: ";
+$numero2= (float) fgets(STDIN);
+
+print "Digite o 3º número: ";
+$numero3= (float) fgets(STDIN);
+
+print "Digite o 4º número: ";
+$numero4= (float) fgets(STDIN);
+
+print "Digite o 5º número: ";
+$numero5= (float) fgets(STDIN);
+
+//Para determinar o maior
+
+if ($numero1>$numero2 && $numero1>$numero3 && $numero1>$numero4 && $numero1>$numero5) 
+    {print "O 1º número é o maior\n";}
+    
+if ($numero2>$numero1 && $numero2>$numero3 && $numero2>$numero4 && $numero2>$numero5) 
+    {print "O 2º número é o maior\n";}
+    
+if ($numero3>$numero1 && $numero3>$numero2 && $numero3>$numero4 && $numero3>$numero5) 
+    {print "O 3º número é o maior\n";}
+    
+if ($numero4>$numero1 && $numero4>$numero2 && $numero4>$numero3 && $numero3>$numero5) 
+    {print "O 4º número é o maior\n";}
+    
+if ($numero5>$numero1 && $numero5>$numero2 && $numero5>$numero3 && $numero5>$numero4) 
+    {print "O 5º número é o maior\n";}
+    
